15e43a7fdc69c6f7140b73f77d075a0d2563a7d8
Noman Shamim <nomanshamim2004@gmail.com>
2024-06-01 23:38:50 +0500

    Still working on modeling...

---
+++ Class 1/src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+const UserModel = mongoose.model('User', UserSchema)
+
+
+module.exports = UserModel
