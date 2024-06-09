98a87d3d0d21c81d97357117542a2f3c9faf0f31
Noman Shamim <nomanshamim2004@gmail.com>
2024-06-09 14:33:59 +0500

    working on todo..

--- Class 1/Server.js
+++ Class 1/Server.js
@@ -5,14 +5,36 @@ const { config } = require('./src/configs/server.config');
 const { corsConfig } = require('./src/configs/cors.config');
 const { route: userRoute } = require('./src/routes/user.routes');
 const { route: todoRoute } = require('./src/routes/todo.routes');
+const { DB_RETRY_LIMIT, DB_RETRY_TIMEOUT } = require('./src/constants/constants');
+
+let connectionRetry = 0
+const connectDB = async () => {
+    try {
+
+        console.log('Establishing DB Connection...');
+        await mongoose.connect(config.dbUri)
+        console.log('DB Connected');
+
+    } catch (error) {
+
+        if (connectionRetry < DB_RETRY_LIMIT) {
+            connectionRetry++
+            console.log(`Reconnnecting to DB ${connectionRetry}/${DB_RETRY_LIMIT}`);
+            await new Promise(resolve => setTimeout(resolve, DB_RETRY_TIMEOUT))
+            await connectDB()
+
+        } else {
+            process.exit()
+        }
+    }
+}
 
 const app = express();
 const PORT = config.appPort;
 
 (async () => {
     try {
-        await mongoose.connect(config.dbUri)
-        console.log('DB Connected');
+        await connectDB()
 
         app.use(cors(corsConfig))
         app.use(express.json())
